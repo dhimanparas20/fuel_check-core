ddb4084d229dea77bdbf2a7f8ab9fe58f0d8d692
dhimanparas20 <dhimanparas20@gmail.com>
2026-03-01 21:42:07 +0530

    Add black, mypy-extensions, packaging, pathspec, platformdirs, and pytokens dependencies.

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("user.urls")),
     path("api/", include("fuel_check.urls")),
     path("login/", TemplateView.as_view(template_name="login.html"), name="login"),
-    path("dashboard/", TemplateView.as_view(template_name="dash.html"), name="dashboard"),
-    path("txn/<int:vehicle_id>/", TemplateView.as_view(template_name="txn.html"), name="txn"),
+    path(
+        "dashboard/", TemplateView.as_view(template_name="dash.html"), name="dashboard"
+    ),
+    path(
+        "txn/<int:vehicle_id>/",
+        TemplateView.as_view(template_name="txn.html"),
+        name="txn",
+    ),
+    path("", RedirectView.as_view(url="/dashboard/", permanent=False), name="index"),
 ]
